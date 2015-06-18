b3fbf461c03288b040894c36da8f712d6b6cff0c
sakthi123 <sakthivel@qadex.com>
2015-06-18 08:50:32 +0000

    new functionality added to get input from users

--- app/controllers/shorturls_controller.rb
+++ app/controllers/shorturls_controller.rb
@@ -1,8 +1,10 @@
 class ShorturlsController < ApplicationController
   def new
+    @shorturl = Shorturl.new
   end
 
   def create
+    
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'shorturls/new'
+  resources 'shorturls', only:[:new, :create, :index]
+  #get 'shorturls/new'
 
-  get 'shorturls/create'
+  #get 'shorturls/create'
 
-  get 'shorturls/index'
+  #get 'shorturls/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
