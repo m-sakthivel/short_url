bc299a2f6421357e5fddc83b83702c4bde5ea95e
sakthi123 <sakthivel@qadex.com>
2015-06-18 10:04:07 +0000

    short url generated and display in index page

--- app/controllers/shorturls_controller.rb
+++ app/controllers/shorturls_controller.rb
@@ -5,18 +5,26 @@ class ShorturlsController < ApplicationController
 
   def create
     if !params[:long_url].blank?
-      
+      regx = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+    if  (params[:long_url] =~ regx) 
+      @shorturl = Shorturl.new
       client = Bitly.client
       @url = client.shorten(params[:long_url])
-      @shorturl = Shorturl.new
+      
       @shorturl.long_url = params[:long_url]
       @shorturl.short_url=@url.short_url
       @shorturl.save
       redirect_to shorturls_path
-      
+    else
+      flash[:error] = "invalid url"
+      redirect_to new_shorturl_path
+    end
     end
   end
 
   def index
+    @shorturls = Shorturl.all
+    
   end
+  
 end
--- app/models/shorturl.rb
+++ app/models/shorturl.rb
@@ -1,2 +1,4 @@
+require 'uri'
 class Shorturl < ActiveRecord::Base
+    validates_format_of :long_url, :with => URI.regexp
 end
